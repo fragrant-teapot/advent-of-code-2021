0abbc927f1a1c405e3b7f9821b1b607c2153c47b
Jakub Łosiowski <jlosiowski@protonmail.com>
2021-12-01 10:05:21 +0100

    day 1

---
+++ day1/day1.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+$input = file_get_contents(__DIR__ . '/input');
+$input = explode(PHP_EOL, $input);
+
+$depthCounter = 0;
+$last = $input[0];
+foreach ($input as $val) {
+    if ($val > $last) {
+        $depthCounter++;
+    }
+
+    $last = $val;
+}
+
+echo('Part 1: ' . $depthCounter . PHP_EOL);
+
+$depthCounter = 0;
+
+$lastWindow = (int)$input[0] + (int)$input[1] + (int)$input[2];
+for ($i = 0; $i <= count($input) - 3; $i++) {
+    $window = (int)$input[$i] + (int)$input[$i + 1] + (int)$input[$i + 2];
+
+    if ($window > $lastWindow) {
+        $depthCounter++;
+    }
+
+    $lastWindow = $window;
+}
+
+echo('Part 2: ' . $depthCounter . PHP_EOL);
